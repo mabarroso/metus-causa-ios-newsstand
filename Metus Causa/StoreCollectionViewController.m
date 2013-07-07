4213524a18c4e7072b9e7320e9ba313ac55f702c
mabarroso <mabarroso@mabarroso.com>
2013-07-07 11:50:25 +0200

    CollectionView cell

--- Metus Causa/StoreCollectionViewController.m
+++ Metus Causa/StoreCollectionViewController.m
@@ -33,9 +33,7 @@
     [super viewDidLoad];
 
     publication = [[Publication alloc] init];
-    
-    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"Cell"];
-    
+        
     // define right bar button items
     refreshButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(loadIssues)];
     UIActivityIndicatorView *loadingActivity = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
@@ -50,6 +48,18 @@
     self.navigationItem.leftBarButtonItem=[[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemTrash target:self action:@selector(trashContent)];
     
     [self loadIssues];
+
+    // Collection
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"Cell"];
+    UINib *cellNib = [UINib nibWithNibName:@"NibCell" bundle:nil];
+    [self.collectionView registerNib:cellNib forCellWithReuseIdentifier:@"cvCell"];
+    
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    [flowLayout setItemSize:CGSizeMake(100, 100)];
+    [flowLayout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    
+    [self.collectionView setCollectionViewLayout:flowLayout];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -60,22 +70,22 @@
 
 #pragma mark - Table View
 
-- (NSInteger)numberOfSectionsInCollectionView: (UICollectionView *)collectionView
-{
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
     return 1;
 }
 
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
-{
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
     return [publication numberOfIssues];
 }
 
+
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    UICollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+
+    UICollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"cvCell" forIndexPath:indexPath];
     
     NSInteger index = indexPath.row;
-    
+/*
     UILabel *titleLabel = (UILabel *)[cell viewWithTag:101];
     NSString *name = [publication name:index];
     name = [name stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
@@ -83,9 +93,27 @@
     name = [name stringByAppendingString:[publication number:index]];
     name = [name stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
     titleLabel.text=name;    
+
+    UILabel *subtitleLabel = (UILabel *)[cell viewWithTag:102];
+    subtitleLabel.text=[publication title:index];
+*/
+    UIImageView *imageView = (UIImageView *)[cell viewWithTag:103];
+    [imageView setImage:[publication coverImage:index]];
+
+//    NSLog(@"%@",name);
+
+
+/*
+    NSString *cellData = @"cvCell";
     
-    NSLog(@"%@",name);
+    static NSString *cellIdentifier = @"cvCell";
     
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    UILabel *titleLabel = (UILabel *)[cell viewWithTag:100];
+    
+    [titleLabel setText:cellData];
+*/
     return cell;
 
 }
